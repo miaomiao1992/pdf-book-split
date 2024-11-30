69598e2f7ecdeb2c19907b95882a361ccb568b92
杨学者 <644284807@qq.com>
2024-11-30 21:22:56 +0800

    2024-11-30-21:22:56-push-miaomiao1992-

--- pdf-split-batch.bat
+++ pdf-split-batch.bat
@@ -1,5 +1,13 @@
+@echo off
+
 set /p input=please enter the pdf path to parse batch: 
 set "input=%input:"=%"
+
+if not exist "%input%" (
+@echo not exist %input% !
+@pause
+exit /b
+)
 
 for %%f in ("%input%\*.pdf") do (
 @echo spliting %%f
